ee142ebf5e1b40ab5c54f30aecbdd0f11a5ba694
Marina Sirenko <msirenko96@gmail.com>
2025-03-13 13:08:24 +0300

    add Makefile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("java")
+    application
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 group = "org.example"
@@ -8,6 +9,8 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+
+application { mainClass.set("org.example.App") }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
